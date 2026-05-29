47cac247387b3c6c1d8416f91079edf0d62ff377
Adalgocode <adarshveer8368@gmail.com>
2026-05-29 12:42:44 +0530

    DAY-1 uploaded

---
+++ DAY-1/Multi_table.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ,i;
+    cout<<" Enter the number = ";
+    cin>>n;
+    for( i=0;i<=10;i++)
+    cout<<n<<  "X"  << i<< "=" <<n*i<<endl;
+    return 0;
+}
